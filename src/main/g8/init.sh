6db939d4514a17b2837c1713a7f035ecc6810fb5
Steve Waldman <swaldman@mchange.com>
2020-02-09 08:52:14 -0800

    Set permissions of files created in 'init.sh'

--- src/main/g8/init.sh
+++ src/main/g8/init.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-mkdir -p secrets/nodepasswd
-touch    secrets/nodepasswd/nodepasswd
+mkdir -p  secrets/nodepasswd
+chmod 700 secrets
+chmod 700 secrets/nodepasswd
+touch     secrets/nodepasswd/nodepasswd
+chmod 600 secrets/nodepasswd/nodepasswd
+
 
